63dfa2925dd653db1df1e1a708f2e28d6eef925a
felix <felix@familie-schuetze.de>
2014-07-31 19:53:27 +0200

    Sieht aus wie ein Schachbrett

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -4,11 +4,14 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -21,6 +24,7 @@ public class GUI extends Application {
 	BorderPane pane = new BorderPane();
 	Box feld = new Box(500, 500, 10);
 	Xform root3D = new Xform();
+	private DoubleProperty zoom = new SimpleDoubleProperty(50);
 
 	public static void main(String args[]) throws Exception {
 		launch(args);
@@ -28,14 +32,14 @@ public class GUI extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// PerspectiveCamera kamera = new PerspectiveCamera(true);
+		PerspectiveCamera kamera = new PerspectiveCamera();
 		// kamera.setFieldOfView(50.0);
 
 		root3D.getChildren().add(feld);
 		pane.setCenter(root3D);
 		PhongMaterial feldMaterial = new PhongMaterial();
-		feldMaterial.setDiffuseColor(Color.WHITE);
-		feldMaterial.setSpecularColor(Color.WHITE);
+		feldMaterial.setDiffuseMap(new Image(this.getClass().getClassLoader()
+				.getResource("gui/bilder/brett.png").toString()));
 		feldMaterial.setBumpMap(new Image(this.getClass().getClassLoader()
 				.getResource("gui/bilder/holz_NRM.png").toString()));
 		feldMaterial.setSpecularMap(new Image(this.getClass().getClassLoader()
@@ -44,7 +48,16 @@ public class GUI extends Application {
 		root3D.rx.setAngle(-70);
 
 		Scene scene = new Scene(pane, 800, 800);
-		scene.setCamera(new PerspectiveCamera());
+		scene.setCamera(kamera);
+
+		kamera.fieldOfViewProperty().bind(zoom);
+
+		scene.setOnScroll(new EventHandler<ScrollEvent>() {
+			@Override
+			public void handle(ScrollEvent event) {
+					zoom.set(zoom.get() + (event.getDeltaY() / 8));
+			}
+		});
 
 		scene.onMouseDraggedProperty().set(new MouseEventHandler());
 		stage.setScene(scene);
@@ -69,7 +82,7 @@ public class GUI extends Application {
 			double mouseX = mouseEvent.getSceneX();
 			double mouseY = mouseEvent.getSceneY();
 
-			root3D.ry.setAngle(mouseY);
+			root3D.rz.setAngle(mouseY);
 			root3D.rx.setAngle(mouseX);
 		}
 
