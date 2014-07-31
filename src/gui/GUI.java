ec0f8438f4b211d17facd4ad0882a8164fcf5f86
felix <felix@familie-schuetze.de>
2014-07-31 16:38:28 +0200

    ZwergenSchach fertig

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,16 +1,24 @@
 package gui;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class GUI extends Application{
 
-	Box myBox = new Box(500,300,200);
+	BorderPane pane=new BorderPane();
+	Box feld = new Box(500,500,15);
 	Xform root3D = new Xform();
 	public static void main(String args[]) throws Exception
 	{
@@ -19,16 +27,31 @@ public class GUI extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		PerspectiveCamera kamera = new PerspectiveCamera(true);
-		kamera.setFieldOfView(50.0);
+		//PerspectiveCamera kamera = new PerspectiveCamera(true);
+		//kamera.setFieldOfView(50.0);
 		
-		root3D.getChildren().add(myBox);
-		
-		Scene scene=new Scene(root3D);
-		scene.setCamera(kamera);
+		root3D.getChildren().add(feld);
+		pane.setCenter(root3D);
+		PhongMaterial feldMaterial=new PhongMaterial();
+		feldMaterial.setDiffuseColor(Color.WHITE);
+	    feldMaterial.setSpecularColor(Color.WHITE);
+		feldMaterial.setBumpMap(new Image(this.getClass().getClassLoader().getResource("gui/bilder/holz_hell.png").toString()));
+		feld.setMaterial(feldMaterial);
+		root3D.rx.setAngle(-70);
+		Scene scene=new Scene(pane,800,800);
+		scene.setCamera(new PerspectiveCamera());
 		stage.setScene(scene);
 		stage.setTitle("Super Schach");
 		stage.getIcons().add(new Image(this.getClass().getClassLoader().getResource("gui/bilder/bauer_schwarz.png").toString()));
 		stage.show();
+		Timeline animation = new Timeline();
+        animation.getKeyFrames().addAll(
+                new KeyFrame(Duration.ZERO,
+                        new KeyValue(root3D.ry.angleProperty(), 0d)
+                ),
+                new KeyFrame(Duration.valueOf("5s"),
+                        new KeyValue(root3D.ry.angleProperty(), 360d)
+                ));
+        animation.play();
 	}
 }
