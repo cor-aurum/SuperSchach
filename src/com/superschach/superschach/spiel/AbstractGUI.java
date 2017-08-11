bcf4f051466d80ab799a5b96239728c7d73d7f12
felix <felix@frodo>
2017-08-11 21:43:17 +0200

    Die DAtenbankklasse im KI Package hat offenbar keiner gebraucht

--- src/com/superschach/superschach/spiel/AbstractGUI.java
+++ src/com/superschach/superschach/spiel/AbstractGUI.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import com.superschach.superschach.ki.Datenbank;
 import com.superschach.superschach.ki.KI;
 
 public abstract class AbstractGUI
